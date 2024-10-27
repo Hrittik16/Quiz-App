25d67a190972e71ebc7b7df47a20e879d8cb8042
Hrittik Das <hrittikdas16@gmail.com>
2024-10-27 15:55:52 +0530

    quiz app added

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      question: "What is 2-1*2 ?",
+      option1: "2",
+      option2: "1",
+      option3: "0",
+      option4: "-1",
+      ans: 3,
+    },
+    {
+      question: "Who invented Calculus ?",
+      option1: "Galileo Galilei",
+      option2: "Isaac Newton",
+      option3: "Leonhard Euler",
+      option4: "Albert Einstein",
+      ans: 2,
+    },
+    {
+      question: "What is the slope of y=2x+5 ?",
+      option1: "5",
+      option2: "1",
+      option3: "0",
+      option4: "2",
+      ans: 4,
+    },
+    {
+      question: "What is d/dx(sin(x)) ?",
+      option1: "cos(x)",
+      option2: "-cos(x)",
+      option3: "sin(x)",
+      option4: "-sin(x)",
+      ans: 1,
+    },
+    {
+      question: "How many diagonals does a hexagon have ?",
+      option1: "6",
+      option2: "8",
+      option3: "9",
+      option4: "10",
+      ans: 3,
+    },
+  ];
